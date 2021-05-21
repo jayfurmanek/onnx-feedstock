b0693dbb2e74e2b2f0bc2957e11687b74e4c5522
Jason Furmanek <furmanek@us.ibm.com>
2021-05-21 10:58:10 -0400

    add 1.7 and 1.8.1

---
+++ onnx-1.7/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# *****************************************************************
+# (C) Copyright IBM Corp. 2019, 2021. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export ONNX_ML=1
+export CONDA_PREFIX="$PREFIX"  # build script looks at this, but not set on
+
+# Let's be explicit with CMake.
+export CMAKE_ARGS="${CMAKE_ARGS} -DProtobuf_PROTOC_EXECUTABLE=$BUILD_PREFIX/bin/protoc -DProtobuf_LIBRARY=$PREFIX/lib/libprotobuf${SHLIB_EXT}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_AR=${AR}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_LINKER=${LD}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_NM=${NM}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OBJCOPY=${OBJCOPY}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_OBJDUMP=${OBJDUMP}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_RANLIB=${RANLIB}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_STRIP=${STRIP}"
+python -m pip install --no-deps --ignore-installed --verbose .
